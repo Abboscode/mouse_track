72d40c568709f35850c2a0b1b5cec0838462d82e
Abbosjon <karshiyevabbosjon@gmail.com>
2024-10-20 16:23:31 +0200

    menu added

--- main.js
+++ main.js
@@ -50,10 +50,30 @@ class Shapes {
 //----
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
-const shape = new Shapes(100, 100, ctx)
+
 //----
 
-shape.drawCircle()
+const mechanics={
+'intialPositon':1,
+    'velocity':1,
+'acceleration':10,
+'energyLost':.2
+
+}
+const shapeSetting={
+
+    'x':10,
+    'y':30,
+}
+
+//fucntoon return shape ,regarding to btn clickec for example ball returns cirlce rec returns rectangule
+
+const ball = (shapeSetting)=>{
+
+return new Shapes(shapeSetting.x,shapeSetting.y ,ctx)
+
+}
+
 
 
 
@@ -61,7 +81,9 @@ shape.drawCircle()
 const animateShapes = {
 
     moveHorizontal(shape) {
-        let translate = 0
+        let velocity = mechanics.velocity
+        let acceleration=mechanics.acceleration
+        const energyLost=mechanics.energyLost
         //flag for that shapes reaches rightCorner 
         let canvasCornerRight = false
         function horizontal() {
@@ -69,17 +91,27 @@ const animateShapes = {
 
             ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-            if (translate < canvas.width*0.9 && !canvasCornerRight) {
-                translate++
-                shape.x = translate
+            if (shape.x > canvas.width-10 ) {
+                canvasCornerRight=true
+
+                //lose energy at contact with corner
+                acceleration-=energyLost
+            }else if (shape.x <10 && canvasCornerRight) {
+                canvasCornerRight=false
+                
+                acceleration=acceleration-energyLost
+            }
+          
+            if (!canvasCornerRight) {
+                
+                shape.x = shape.x+velocity+(1/2)*acceleration
+            
 
             } else {
-                canvasCornerRight=true
-                translate--
-                shape.x = translate
-                if(translate <canvas.width*.1 && canvasCornerRight){
-                    canvasCornerRight=false
-                } 
+        
+                
+                shape.x = shape.x-velocity-(1/2)*acceleration
+               
             }
             shape.drawCircle(10)
 
@@ -91,5 +123,6 @@ const animateShapes = {
 
 }
 
-
+const shape = ball(shapeSetting);
+console.log(typeof(shape))
 animateShapes.moveHorizontal(shape);
